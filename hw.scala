922825f4b6db639b02d528d28dca7cac6e0fb740
robccharles <robccharles@users.noreply.github.com>
2017-09-06 08:14:39 -0800

    Update hw.scala

--- hw.scala
+++ hw.scala
@@ -1,4 +1,11 @@
+//
+//  file hw.scala
+//  It is a test scala program
+//
+//  author Roberto Herrera id robccharles
+//
+//  date: sept 5 2017.
 
 object Hi {
-            def main(args: Array[String]) = println("Hi!")
+            def main(args: Array[String]) = println("Hi! Scala again")
 }
