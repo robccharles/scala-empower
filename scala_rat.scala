4db27ecccd7e11a67395947ee05fc9c87736aefa
Roberto H <robccharles@citedi.mx>
2020-06-03 00:55:00 -0800

    Add last fuctions

--- scala_rat.scala
+++ scala_rat.scala
@@ -4,3 +4,8 @@ def numer(x: List[Int]):Int = x.head
 def denom(x: List[Int]):Int = x.tail.head
 // function add
 def add_rat(x: List[Int], y: List[Int]): List[Int] = make_rat ((numer (x)* denom(y)+ numer( y) * denom ( x)),  denom (x)*denom (y)  )
+def print_rat(x: List[Int]) = println(" "+numer(x)/gcd(numer(x),denom(x))+"\n---\n "+denom(x)/gcd(numer(x),denom(x)))
+def sub_rat(x: List[Int], y: List[Int]): List[Int] = make_rat ((numer (x)* denom(y)- numer( y) * denom ( x)),  denom (x)*denom (y))
+def mul_rat(x: List[Int], y: List[Int]): List[Int] = make_rat (numer (x)* numer( y) ,  denom (x)*denom (y)  )
+def div_rat(x: List[Int], y: List[Int]): List[Int] = make_rat (numer (x)* denom( y) ,  denom (x)*numer (y)  )
+def equal_rat(x: List[Int], y: List[Int]) = if (  numer (x)* denom( y) ==  denom (x)*numer (y)  ) true else false
