7dd151ee3348c4e302f709fd6188778d87d33f31
Roberto H <robccharles@citedi.mx>
2020-06-03 00:45:13 -0800

    Add add_rat

--- scala_rat.scala
+++ scala_rat.scala
@@ -2,5 +2,5 @@ def gcd (x: Integer, y: Integer): Integer =  if (x==y)  x else if (x > y) gcd((x
 def make_rat(n: Int, d: Int) = n::d::Nil
 def numer(x: List[Int]):Int = x.head
 def denom(x: List[Int]):Int = x.tail.head
-
-//def add_rat(x: List[Integer], y: List[Integer]): List[Integer] = make_rat ((numer (x)* denom(y)+ numer( y) * denom ( x)),  denom (x)*denom (y)  )
+// function add
+def add_rat(x: List[Int], y: List[Int]): List[Int] = make_rat ((numer (x)* denom(y)+ numer( y) * denom ( x)),  denom (x)*denom (y)  )
